fc0cc9aac19da5d7311dbe4e9f0eaff0773388f2
Chingiz <olzhabaev.sh@outlook.com>
2017-03-13 15:16:08 +0600

    add new projs

---
+++ Excalibur/Core/Interfaces/IBaseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IBaseManager<TSource, TKey>
+    {
+        IStore<TSource, TKey> Store { get; }
+
+        TSource Find(TKey key);
+    }
+}
---
+++ Excalibur/Core/Interfaces/IStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IStore<TSourse, TKey>: IDisposable
+    {
+        Task<TSourse> GetElement(TKey Key);
+
+        Task<TSourse> GetElement(Func<TSourse, bool> predicate);
+
+        Task<TSourse> GetElement(List<Func<TSourse, bool>> predicates);
+
+        Task<ICollection<TSourse>> GetCollection();
+
+        Task<ICollection<TSourse>> GetCollection(Func<TSourse, bool> predicate);
+
+        Task<ICollection<TSourse>> GetCollection(List<Func<TSourse, bool>> predicate);
+
+        Task<TSourse> CreateElement(TKey element);
+
+        Task<TSourse> UpdateElement(TKey key, TSourse element);
+
+        Task<TSourse> DeleteElement(TSourse key);
+    }
+}
