cada1bb108a18de703fd2d7aecf935bb31805f8a
Chingiz <olzhabaev.sh@outlook.com>
2017-03-14 09:59:34 +0600

    fix

--- Excalibur/WebAPI.Admin/App_Start/UnityConfig.cs
+++ Excalibur/WebAPI.Admin/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
+using Core.Admin.Models;
 
 namespace WebAPI.Admin.App_Start
 {
@@ -34,7 +35,7 @@ namespace WebAPI.Admin.App_Start
         {
             // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
             // container.LoadConfiguration();
-
+            container.RegisterType<ApplicationDbContext, ApplicationDbContext>();
             // TODO: Register your types here
             // container.RegisterType<IProductRepository, ProductRepository>();
         }
