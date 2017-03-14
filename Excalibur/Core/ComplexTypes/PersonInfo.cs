b1ccd3450ce469a08aabf8a0bca6d3c0f67e1044
Chingiz <olzhabaev.sh@outlook.com>
2017-03-14 10:42:24 +0600

    added complex types

---
+++ Excalibur/Core/ComplexTypes/PersonInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ComplexTypes
+{
+    [ComplexType]
+    public class PersonInfo
+    {
+        [Required]
+        public string IIN { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+    }
+}
