cb985e97a7f71947671987fdfdda9bf4d6e71375
Chingiz <olzhabaev.sh@outlook.com>
2017-03-14 10:42:24 +0600

    added complex types

---
+++ Excalibur/Core/ComplexTypes/ContinuousAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ComplexTypes
+{
+    [ComplexType]
+    public class ContinuousAction
+    {
+        public DateTime? PlanStartDate { get; set; }
+        public DateTime? PlanFinishDate { get; set; }
+        public DateTime? FactStartDate { get; set; }
+        public DateTime? FactFinishDate { get; set; }
+
+        public bool? IsFinished { get; set; }
+    }
+}
---
+++ Excalibur/Core/ComplexTypes/ItemChangeInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ComplexTypes
+{
+    [ComplexType]
+    public class ItemChangeInfo
+    {
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedUserId { get; set; }
+
+        public DateTime? LastUpdatedDate { get; set; }
+        public string LastUpdatedUserId { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+        public string DeletedUserId { get; set; }
+
+        public bool? IsDeleted { get; set; }
+    }
+}
