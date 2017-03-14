a1e8feb0afdedc326d432df6f93e9facad25e14d
Chingiz <olzhabaev.sh@outlook.com>
2017-03-14 14:31:41 +0600

    Update connection string

--- Excalibur/Core.Admin/ViewModels/ApplicationUserVM.cs
+++ Excalibur/Core.Admin/ViewModels/ApplicationUserVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.ComplexTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,7 @@ namespace Core.Admin.ViewModels
         public string Email { get; set; }
 
         public string HashPwd { get; set; }
+
+        public PersonInfo PersonInfo { get; set; }
     }
 }
