3640890b03fa731de3bc01b3e6040710740f6256
Samat Yeshenkulov <yeshenk.s@gmail.com>
2017-03-15 15:59:09 +0600

    finish ApplicationStore

--- Excalibur/Core.Admin/Stores/ApplicationStore.cs
+++ Excalibur/Core.Admin/Stores/ApplicationStore.cs
@@ -6,54 +6,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq.Expressions;
+using System.Data.Entity;
 
 namespace Core.Admin.Stores
 {
     class ApplicationStore : IStore<Application, string>
     {
+        private readonly ApplicationDbContext _dbContext;
+        public ApplicationStore(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public Task<Application> CreateElement(Application element)
         {
-            throw new NotImplementedException();
+            _dbContext.Applications.Add(element);
+            _dbContext.SaveChanges();
+            return Task.FromResult(element);
         }
 
-        public Task<Application> DeleteElement(string key)
+        public Task<bool> DeleteElement(string key)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var application = _dbContext.Applications.Find(key);
+                if(application != null)
+                {
+                    _dbContext.Applications.Remove(application);
+                    _dbContext.SaveChanges();
+                    return Task.FromResult(true);
+                }
+                return Task.FromResult(false);
+            }
+            catch (Exception)
+            {
+                return Task.FromResult(false);
+            }
         }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
+        
         public Task<ICollection<Application>> GetCollection()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(_dbContext.Applications as ICollection<Application>);
         }
 
         public Task<ICollection<Application>> GetCollection(Expression<Func<Application, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(_dbContext.Applications.Where(predicate) as ICollection<Application>);
         }
 
         public Task<Application> GetElement(string Key)
         {
-            throw new NotImplementedException();
+            return _dbContext.Applications.FindAsync(Key);
         }
 
         public Task<Application> GetElement(Expression<Func<Application, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return _dbContext.Applications.FirstOrDefaultAsync(predicate);
         }
 
         public Task<Application> GetElementByExpression(Expression<Func<Application, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return _dbContext.Applications.FirstOrDefaultAsync(predicate);
         }
 
         public Task<Application> UpdateElement(string key, Application element)
         {
-            throw new NotImplementedException();
+            _dbContext.Applications.Attach(element);
+            _dbContext.Entry(element).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return Task.FromResult(element);
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
         }
     }
 }
